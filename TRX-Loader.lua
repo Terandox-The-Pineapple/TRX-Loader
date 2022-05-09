b211d232d8cc8443b228af26ab15b457e0a23b80
Terandox <rene-gilbert99@web.de>
2022-05-09 09:07:18 +0200

    Added main file
    Initial push of main file

---
+++ TRX-Loader.lua
@@ -0,0 +1,56 @@
+function init()
+	print("Installations: ")
+	print("-------------------------")
+	print("1: Storage-System")
+	print("2: Mining-System")
+	print("-------------------------")
+	io.write("Choose Number: ")
+	local target = tonumber(read())
+	if target==1 then
+		initStorage()
+	elseif target == 2 then
+		initMiningSystem()
+	else
+		print("Input Invalid")
+	end
+end
+
+function initMiningSystem()
+	print("Programms: ")
+	print("-------------------------")
+	print("1: Turtle")
+	print("-------------------------")
+	io.write("Choose Number: ")
+	local target = tonumber(read())
+	if target == 1 then
+		shell.run("pastebin get qqRYGimt TRX-Miner")
+		shell.run("TRX-Miner")
+	else
+		print("Input Invalid")
+	end
+end
+
+function initStorage()
+	print("Programms: ")
+	print("-------------------------")
+	print("1: Server")
+	print("2: Tablet")
+	print("3: Turtle")
+	print("-------------------------")
+	io.write("Choose Number: ")
+	local target = tonumber(read())
+	if target==1 then
+		shell.run("pastebin get https://pastebin.com/7pZUAgDy startup")
+		shell.run("reboot")
+	elseif target==2 then
+		shell.run("pastebin get https://pastebin.com/KvJUYPip startup")
+		shell.run("reboot")
+	elseif target==3 then
+		shell.run("pastebin get https://pastebin.com/XeYSaY2K startup")
+		shell.run("reboot")
+	else
+		print("Input Invalid")
+	end
+end
+
+init()
